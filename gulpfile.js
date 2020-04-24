c12117c60ef756949367c437e6699afa58bb9396
ellavrubel <webella@protonmail.com>
2020-04-24 10:58:15 +0300

    gulp has been set

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,13 @@ const gulp = require('gulp'),
       rename = require('gulp-rename'),
       uglify = require('gulp-uglify'),
       concat = require('gulp-concat'),
+      del = require('del'),
       browserSync = require('browser-sync');
+
+
+    gulp.task('clean', async function(){
+        del.sync('dist')
+    });
 
 
 
@@ -17,9 +23,60 @@ const gulp = require('gulp'),
         .pipe(browserSync.reload({stream: true}))
 });
 
+    gulp.task('html', function () {
+        return gulp.src('app/*.html')
+            .pipe(browserSync.reload({stream: true}))
+    });
+
+    gulp.task('css', function () {
+        return gulp.src(
+            'node_modules/normalize.css/normalize.css'
+        )
+            .pipe(concat('_libs.scss'))
+            .pipe(gulp.dest('app/scss'))
+            .pipe(browserSync.reload({stream: true}))
+    });
 
 
+    gulp.task('script', function () {
+        return gulp.src('app/js/*.js')
+            .pipe(browserSync.reload({stream: true}))
+    });
 
+    gulp.task('js', function () {
+        return gulp.src('app/*.js')
+            .pipe(concat('libs.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('app/js'))
+            .pipe(browserSync.reload({stream: true}))
+    });
+
+    gulp.task('browser-sync', function() {
+        browserSync.init({
+            server: {
+                baseDir: "app/"
+            }
+        });
+    });
+
+
+    gulp.task('export', async function () {
+        let buildHtml = gulp.src('app/**/*.html')
+            .pipe(gulp.dest('dist'));
+
+        let buildCss = gulp.src('app/css/**/*.css')
+            .pipe(gulp.dest('dist/css'));
+
+        let buildJs = gulp.src('app/js/**/*.js')
+            .pipe(gulp.dest('dist/js'));
+
+        let buildFonts = gulp.src('app/fonts/**/*.*')
+            .pipe(gulp.dest('dist/fonts'));
+
+        let buildImg = gulp.src('app/img/**/*.*')
+            .pipe(gulp.dest('dist/img'));
+
+    });
 
 
 
@@ -28,3 +85,10 @@ gulp.task('watch', function () {
     gulp.watch('app/*.html', gulp.parallel('html'));
     gulp.watch('app/js/*.js', gulp.parallel('script'));
 });
+
+
+
+
+
+gulp.task('build', gulp.series('clean', 'export'));
+gulp.task('default', gulp.parallel('css', 'scss', 'js', 'browser-sync', 'watch'));
