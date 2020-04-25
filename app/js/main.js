12d6df041e254a033a2aa59e691fdf5c97e705e1
ellavrubel <webella@protonmail.com>
2020-04-25 17:24:33 +0300

    section head @media 640 done

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,8 @@ $(function () {
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
+        nextArrow: '<button type="button" class="slick-next slick-arrow"><img src="../img/next.svg" alt=""></button>',
+        prevArrow: '<button type="button" class="slick-prev slick-arrow"><img src="../img/prev.svg" alt=""></button>',
         responsive: [
             {
                 breakpoint: 1024,
