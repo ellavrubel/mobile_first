8edaf36d0cec5d0555870d33cc8b178c76184650
ellavrubel <webella@protonmail.com>
2020-05-05 13:43:27 +0300

    quote section in prog

--- app/js/main.js
+++ app/js/main.js
@@ -45,3 +45,25 @@
 
     //
 
+    $(function () {
+
+        $('.quote-slider').slick({
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                    }
+                },
+                {
+                    breakpoint: 640,
+                    settings: {
+
+                    }
+                },
+            ]
+        });
+    });
