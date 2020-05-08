00e3ace4896b87074fddc13324df8a1111b3d8c9
ellavrubel <webella@protonmail.com>
2020-05-08 08:57:57 +0300

    dist

---
+++ dist/js/main.js
@@ -0,0 +1,80 @@
+    $(function () {
+
+        $('.top-slider').slick({
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            nextArrow: '<button type="button" class="slick-next slick-arrow"><img src="../img/next.svg" alt=""></button>',
+            prevArrow: '<button type="button" class="slick-prev slick-arrow"><img src="../img/prev.svg" alt=""></button>',
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                    }
+                },
+                {
+                    breakpoint: 640,
+                    settings: {
+                        arrows: false
+
+                    }
+                },
+            ]
+        });
+    });
+
+
+
+
+    //  Projects, hover img
+
+
+        $('.projects__item').hover(function () {
+            $(this).toggleClass('active-visible');
+
+        });
+
+        $('.projects__item').hover(function () {
+            $(this).find('.projects__active').fadeIn(500);
+        }, function () {
+            $('.projects__active').fadeOut(300);
+        });
+
+
+
+
+    //
+
+    $(function () {
+
+        $('.quote-slider').slick({
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                    }
+                },
+                {
+                    breakpoint: 640,
+                    settings: {
+
+                    }
+                },
+            ]
+        });
+
+
+
+    //    кнопка ВВЕРХ
+
+        $('#up__btn').click(function (e) {
+            e.preventDefault();
+            $('html, body').animate({scrollTop:0}, 3000);
+
+        });
+
+    });
